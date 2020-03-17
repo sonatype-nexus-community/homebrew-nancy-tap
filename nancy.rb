3e3712a55bdc830a2baa1b4a70741ebeaa44d7d8
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-17 11:36:44 -0800

    Brew formula update for nancy version v0.1.11

--- nancy.rb
+++ nancy.rb
@@ -2,16 +2,16 @@
 class Nancy < Formula
   desc "A tool to check for vulnerabilities in your Golang dependencies, powered by Sonatype OSS Index"
   homepage "https://github.com/sonatype-nexus-community/nancy"
-  version "0.1.10"
+  version "0.1.11"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sonatype-nexus-community/nancy/releases/download/v0.1.10/nancy-darwin.amd64-v0.1.10.tar.gz"
-    sha256 "7a6559d902092176a64976672022fd61b1ceed8b3e46a61d2131c1be88b6feb9"
+    url "https://github.com/sonatype-nexus-community/nancy/releases/download/v0.1.11/nancy-darwin.amd64-v0.1.11.tar.gz"
+    sha256 "1e762c957273120d4a0412151529e63f8a942e92cac94cd89a9e4d6b07092a0b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v0.1.10/nancy-linux.amd64-v0.1.10.tar.gz"
-      sha256 "f6cc20d0e1d694251e3e02920bdf1009b624f588afd43b3027dbef5f6c61aa2a"
+      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v0.1.11/nancy-linux.amd64-v0.1.11.tar.gz"
+      sha256 "f8cb76988dedda5d0d88faa7408ef97ca999e15b3dbc944506c64041dd4cbb09"
     end
   end
 
