70533a4d3f80803ce6c8486827760308c233ddb4
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-22 12:46:24 -0800

    Brew formula update for nancy version v1.0.24

--- nancy.rb
+++ nancy.rb
@@ -5,28 +5,28 @@
 class Nancy < Formula
   desc "A tool to check for vulnerabilities in your Golang dependencies, powered by Sonatype OSS Index"
   homepage "https://github.com/sonatype-nexus-community/nancy"
-  version "1.0.23"
+  version "1.0.24"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.23/nancy-v1.0.23-darwin-amd64.tar.gz"
-      sha256 "109803527d04fa7d1dd9e2783dedee6c73876cba8528f66dd2b42180792583a8"
+      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.24/nancy-v1.0.24-darwin-amd64.tar.gz"
+      sha256 "9c2247c5ee1324a02887a347d58193b71083a89610f73d9367974978b4e98ec1"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.23/nancy-v1.0.23-darwin-arm64.tar.gz"
-      sha256 "b7302e17aa17723e42c32a8bb5084e054801f670afc07e03e9b761706b9af2ac"
+      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.24/nancy-v1.0.24-darwin-arm64.tar.gz"
+      sha256 "aa7b660b9820c1361e6e7a6225bfb129f478778e10956a888ea243a008d624b1"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.23/nancy-v1.0.23-linux-amd64.tar.gz"
-      sha256 "f822d3fe192d7cb355584d4b0a09314d79e5d8c0cdb8e6bd873146080e9f9acd"
+      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.24/nancy-v1.0.24-linux-amd64.tar.gz"
+      sha256 "1ac325c173a50a40a3196eded7fb7adf7e26751710ecf167de0b96f7731bd39f"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.23/nancy-v1.0.23-linux-arm64.tar.gz"
-      sha256 "880b8fa35a845dc289529e53bff4323cac73d3b4eb7d837ab727c62f637540a6"
+      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.24/nancy-v1.0.24-linux-arm64.tar.gz"
+      sha256 "112944dab1971be00ceef9ec9d22eae9bfd0b94a1ff30f508473d5a60697a724"
     end
   end
 
