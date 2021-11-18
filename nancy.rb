bb72fc3f9535add6c09a20558e26b728a1cb5998
goreleaserbot <goreleaser@carlosbecker.com>
2021-11-18 12:50:44 -0900

    Brew formula update for nancy version v1.0.29

--- nancy.rb
+++ nancy.rb
@@ -5,20 +5,20 @@
 class Nancy < Formula
   desc "A tool to check for vulnerabilities in your Golang dependencies, powered by Sonatype OSS Index"
   homepage "https://github.com/sonatype-nexus-community/nancy"
-  version "1.0.28"
+  version "1.0.29"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.28/nancy-v1.0.28-darwin-arm64.tar.gz"
-      sha256 "27de29749fbcf154accb04ac893e78d015a1142baf7d05265969fe6c30ec9c4f"
+      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.29/nancy-v1.0.29-darwin-arm64.tar.gz"
+      sha256 "1182f0b07b656b36dcce89609166f4747dd2906d7e03fe4eb8f5843da7b801fd"
 
       def install
         bin.install "nancy"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.28/nancy-v1.0.28-darwin-amd64.tar.gz"
-      sha256 "12d74fb736ad57a9010058c52aa2f2c9a784a6e1b599c447ab6233bd3ea22db0"
+      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.29/nancy-v1.0.29-darwin-amd64.tar.gz"
+      sha256 "8eb0b69db5dfa453dd80b2ce5a6da16dd287dcb1dd709002a446b250ac284f44"
 
       def install
         bin.install "nancy"
@@ -28,16 +28,16 @@ class Nancy < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.28/nancy-v1.0.28-linux-arm64.tar.gz"
-      sha256 "5061c751e67349d96da5a7560c015711c94068496e5f655b3b59a5002feb81d5"
+      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.29/nancy-v1.0.29-linux-arm64.tar.gz"
+      sha256 "6ed7eeaaa1cf8fe3a98fac2007a351d715d78f44ca9a838baaf54c241f7365f7"
 
       def install
         bin.install "nancy"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.28/nancy-v1.0.28-linux-amd64.tar.gz"
-      sha256 "66dd186572980333a9fca1bc9722547696d15a5187706c3a8e399994f9a18bac"
+      url "https://github.com/sonatype-nexus-community/nancy/releases/download/v1.0.29/nancy-v1.0.29-linux-amd64.tar.gz"
+      sha256 "6f8dec1437157f5c366ad3975c759689923a447293324921a990da6204afd313"
 
       def install
         bin.install "nancy"
